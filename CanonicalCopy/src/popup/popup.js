1409a8c621cefd13de408a68bb33e24943d35857
Miff <miff@miffthefox.info>
2023-09-12 19:13:55 -0400

    This was surprisingly easy to port to Firefox.

--- CanonicalCopy/src/popup/popup.js
+++ CanonicalCopy/src/popup/popup.js
@@ -1,34 +1,35 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const urlField = document.getElementById("urlField");
-  let canCopy = false;
+    const urlField = document.getElementById("urlField");
+    const br = browser || chrome;
+    let canCopy = false;
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if (!tabs[0] || !/^https?:/.test(tabs[0].url)){
-      document.querySelector("form").classList.add("unavailable");
-      urlField.value = "(Could not get URL)";
-    } else {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {
-          code: '(function(){ const link = document.querySelector("link[rel=\'canonical\']"); return link && link.href ? link.href : ""+window.location; })()'
-        }, function(result) {
-          urlField.value = result;
-          urlField.focus();
-          urlField.select();
+    br.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (!tabs[0] || !/^https?:/.test(tabs[0].url)) {
+            document.querySelector("form").classList.add("unavailable");
+            urlField.value = "(Could not get URL)";
+        } else {
+            br.tabs.executeScript(
+                tabs[0].id,
+                {
+                    code: '(function(){ const link = document.querySelector("link[rel=\'canonical\']"); return link && link.href ? link.href : ""+window.location; })()'
+                }, function (result) {
+                    urlField.value = result;
+                    urlField.focus();
+                    urlField.select();
+                }
+            );
+            canCopy = true;
         }
-      );
-      canCopy = true;
-    }
-  });
+    });
 
-  document.querySelector("form").addEventListener("submit", e => {
-    if (canCopy){
-      urlField.focus();
-      urlField.select();
-      document.execCommand("copy");
-    }
-    e.preventDefault();
-  });
+    document.querySelector("form").addEventListener("submit", e => {
+        if (canCopy) {
+            urlField.focus();
+            urlField.select();
+            document.execCommand("copy");
+        }
+        e.preventDefault();
+    });
 });
